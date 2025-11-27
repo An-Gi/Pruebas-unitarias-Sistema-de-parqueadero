bc2c5dee47d90568239dc567bcca7897dd8dcce0
Angie Esquivel López <an-g@MacBook-Air-de-Angie.local>
2025-11-27 11:35:18 -0600

    Cambios min

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,24 +1,20 @@
-// cypress/support/commands.js
-
 Cypress.Commands.add('loginAdmin', (usuario, password) => {
   cy.visit('/'); 
 
-  // 1. Campo Usuario (Basado en tu imagen)
-  // Buscamos por name="usuario" o el placeholder si el name es difícil
-  cy.get('input[name="usuario"]') // Si falla, intenta: cy.get('input[placeholder="Usuario"]')
+  cy.get('input[name="usuario"]') 
     .should('be.visible')
     .clear()
     .type(usuario);
 
-  // 2. Campo Contraseña
-  cy.get('input[name="password"]') // Si falla, intenta: cy.get('input[type="password"]')
+
+  cy.get('input[name="password"]') 
     .should('be.visible')
     .clear()
     .type(password);
 
-  // 3. Botón Iniciar Sesión
+
   cy.get('button[type="submit"]').click();
 
-  // Validar que entramos
+
   cy.url().should('not.include', '/login');
 });
